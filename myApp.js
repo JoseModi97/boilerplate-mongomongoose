4ad0be2f44597d4c2847486b677f25a838cbb793
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-07-20 10:29:23 +0300

    Add Mongoose connection and Person model
    Established a connection to MongoDB using Mongoose and defined a Person schema and model with name, age, and favoriteFoods fields.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-let Person;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 👇 Add your schema and model HERE
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
